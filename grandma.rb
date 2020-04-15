cef1dead4ca178655e50b77c59e78823f3054b7f
Joann Brady <jbrady11934@optimum.net>
2020-04-15 19:06:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@
 def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase !phrase.upcase
+  elsif phrase !"I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase !phrase
     return "NO, NOT SINCE 1938!"
